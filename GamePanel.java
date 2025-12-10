fc3f05096cce43ba42256194393780e55a84ee43
ASimpson456 <132151204+ASimpson456@users.noreply.github.com>
2025-12-10 12:18:48 +0300

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -32,56 +32,7 @@ public class GamePanel extends JPanel {
         timer = new Timer(DELAY, null);
         timer.start(); }
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        draw(g); }
-    public void draw(Graphics g) {
-        if (running) { // Рисуем яблоко
-            g.setColor(Color.red);
-            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-            // Рисуем змейку
-            for (int i = 0; i < bodyParts; i++) {
-                if (i == 0) {
-                    g.setColor(Color.green);
-                } else {
-                    g.setColor(new Color(45, 180, 0));
-                }
-                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-            }
-            // Отображаем счет
-            g.setColor(Color.white);
-            g.setFont(new Font("Ink Free", Font.BOLD, 40));
-            FontMetrics metrics = getFontMetrics(g.getFont());
-            g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: " + applesEaten)) / 2, g.getFont().getSize());
-        } else {
-        }
-    }
-      // Остальные методы (newApple, move, checkApple, checkCollisions, gameOver, actionPerformed) // будут реализованы аналогично
-    public class MyKeyAdapter extends KeyAdapter {
-        @Override
-        public void keyPressed(KeyEvent e) {
-            switch (e.getKeyCode()) {
-                case KeyEvent.VK_LEFT:
-                    if (direction != 'R') {
-                    direction = 'L';
-                }
-                break;
-                case KeyEvent.VK_RIGHT:
-                    if (direction != 'L') {
-                        direction = 'R';
-                    }
-                break;
-                case KeyEvent.VK_UP:
-                    if (direction != 'D') {
-                        direction = 'U';
-                    }
-                break;
-                case KeyEvent.VK_DOWN:
-                    if (direction != 'U') {
-                        direction = 'D';
-                    }
-                break;
-            }
-        }
     }
 
 }
+
